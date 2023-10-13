3b00fcae9963af152d72eb808de860f4e0e9d247
hallowslab <hallow.tech.lab@gmail.com>
2023-10-13 16:59:09 +0100

    Added an unathorized component

---
+++ src/routes/unauthorized.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const UnauthorizedAccess = () => {
+    return (
+      <div>
+        <p>Unauthorized access. You do not have permission to view this page.</p>
+      </div>
+    );
+}
